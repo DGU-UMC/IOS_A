1c1c67c31fe46bce84a2ebfe838fa43014f5a9f0
Doyeon Kim <ehdus022191@dgu.ac.kr>
2025-09-15 22:28:53 +0900

    [Chore] MegaBoxApp

---
+++ dodo/MegaBox/MegaBox/App/MegaBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MegaBoxApp.swift
+//  MegaBox
+//
+//  Created by 김도연 on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MegaBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
