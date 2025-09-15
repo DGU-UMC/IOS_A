9806610fc48be60e6d0fbba2bfa4ab220d8cd78e
Doyeon Kim <ehdus022191@dgu.ac.kr>
2025-09-15 22:29:40 +0900

    [Feat] SplashView

---
+++ dodo/MegaBox/MegaBox/App/SplashView.swift
@@ -0,0 +1,21 @@
+//
+//  SplashView.swift
+//  MegaBox
+//
+//  Created by 김도연 on 9/15/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        ZStack(alignment: .center) {
+            Color.white
+            Image(.megaBoxLogo)
+        }
+    }
+}
+
+#Preview {
+    SplashView()
+}
