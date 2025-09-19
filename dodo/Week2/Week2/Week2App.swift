fe8ff288c9fcf666f434b1154434a1dd294ee2a9
Doyeon Kim <ehdus022191@dgu.ac.kr>
2025-09-19 21:49:42 +0900

    Week2

---
+++ dodo/Week2/Week2/Week2App.swift
@@ -0,0 +1,17 @@
+//
+//  Week2App.swift
+//  Week2
+//
+//  Created by 김도연 on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Week2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
